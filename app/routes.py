d4d5c139072514cb0d8a38b07cc9fe7da9b7a27e
derekcodingtemple <derekh@codingtemple.come>
2021-09-14 11:24:47 -0500

    injecting data into home.html

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,32 @@ from flask import render_template
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    posts = [
+        {
+            'first_name': 'Derek', 
+            'last_name': 'Hawkins', 
+            'body': 'This is the first post',
+            'date_posted': 9
+        },
+        {
+            'first_name': 'Lucas', 
+            'last_name': 'Lang', 
+            'body': 'This is the second post',
+            'date_posted': 10
+        },
+        {
+            'first_name': 'Ripal', 
+            'last_name': 'Patel', 
+            'body': 'This is the third post',
+            'date_posted': 6
+        }
+    ]
+
+    context = {
+        'posts': posts
+    }
+    # return render_template('home.html', body='This is the first post', first_name='Derek', last_name='Lang', date_posted=9)
+    return render_template('home.html', **context)
 
 @app.route('/about')
 def about():
